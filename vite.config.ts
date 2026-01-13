152f27cfa6b4f7a5ff1ac04ade847c573c129f7a
robsonbercik <robsonbercik.computers@gmail.com>
2026-01-13 06:57:23 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    // To jest kluczowe: przekazuje zmienną z serwera Vercel do kodu przeglądarki
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 });
